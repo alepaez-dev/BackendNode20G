f3f977755beffa768e8324af41dbbd8e86454ca1
alepaez-dev <alepaezdev@gmail.com>
2022-08-10 19:36:11 -0500

    feat: appendFile

--- filesystem.js
+++ filesystem.js
@@ -1,26 +1,40 @@
 const fs = require("fs")
 
 // CRUD -> base de datos -> sistema archivos
-
+// json
+// mongodb
 
 // Create
 // Read
+// Actualizar
+// Eliminar
 
-fs.writeFile("text1.txt", "Archivo creado desde node", "utf8", (err) => {
-  // Logica negativa
+// fs.writeFile("text1.txt", "Archivo creado desde node", "utf8", (err) => {
+//   // Logica negativa
+//   if(err) {
+//     console.log("err", err)
+//     return //Salte
+//   }
+
+//   // Si no, todo lo demas
+//   console.log("El archivo fue creado exito")
+// })
+
+// fs.readFile("ejercicios.js", "utf-8", (err, data) => {
+//   // Logica negativa
+//   if(err) throw err
+
+//   // Continuamos con nuestra vida
+//   console.log(data)
+// })
+// Put -> reemplaza
+// Patch -> Actualiza
+
+fs.appendFile("text1.txt", "\nEsto es NUEVO", "utf8", (err) => {
   if(err) {
     console.log("err", err)
-    return //Salte
+    return
   }
 
-  // Si no, todo lo demas
-  console.log("El archivo fue creado exito")
-})
-
-fs.readFile("ejercicios.js", "utf-8", (err, data) => {
-  // Logica negativa
-  if(err) throw err
-
-  // Continuamos con nuestra vida
-  console.log(data)
+  console.log("Tu archivo fue actualizado con exito")
 })
